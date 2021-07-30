96feb571e7636b52d384d83811434e52e437a05a
Nate Boyette <nateboyette@icloud.com>
2021-07-30 01:19:25 -0700

    ex01ec03

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,7 +4,8 @@
 import * as React from 'react'
 
 function countReducer(state, action) {
-  return {...state, ...action}
+  const newState = typeof action === 'function' ? action(state) : action
+  return {...state, ...newState}
 }
 
 function Counter({initialCount = 0, step = 1}) {
@@ -13,7 +14,8 @@ function Counter({initialCount = 0, step = 1}) {
 })
 
   const {count} = state
-  const increment = () => setState({count: count + step})
+const increment = () =>
+  setState(currentState => ({count: currentState.count + step}))
   return <button onClick={increment}>{count}</button>
 }
 
