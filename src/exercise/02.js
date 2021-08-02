f72d55952541fa07a48bc394a783e3d3f32311df
Nate Boyette <nateboyette@icloud.com>
2021-08-02 00:56:01 -0700

    ex02ec03

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -10,6 +10,24 @@ import {
   PokemonErrorBoundary,
 } from '../pokemon'
 
+function useSafeDispatch(dispatch) {
+  const mountedRef = React.useRef(false)
+
+  React.useEffect(() => {
+    mountedRef.current = true
+    return () => (mountedRef.current = false)
+  }, [])
+
+  return React.useCallback(
+    (...args) => {
+      if (mountedRef.current) {
+        dispatch(...args)
+      }
+    },
+    [dispatch],
+  )
+}
+
 function asyncReducer(state, action) {
   switch (action.type) {
     case 'pending': {
@@ -35,18 +53,7 @@ function useAsync(initialState) {
     ...initialState,
   })
 
-  const mountedRef = React.useRef(false)
-
-  React.useEffect(() => {
-    mountedRef.current = true
-    return () => (mountedRef.current = false)
-  }, [])
-
-  const dispatch = React.useCallback((...args) => {
-    if (mountedRef.current) {
-      unsafeDispatch(...args)
-    }
-  }, [])
+  const dispatch = useSafeDispatch(unsafeDispatch)
 
   const run = React.useCallback(promise => {
     if (!promise) {
